5c41b4b7db932d76c216e56894b0a7868f7f2186
elektrus230 <dinisluna2@gmail.com>
2015-12-12 17:25:28 +0000

    prcessor
    added processor stuff

--- Simplex/src/simplex/DataProcessing.java
+++ Simplex/src/simplex/DataProcessing.java
@@ -17,34 +17,110 @@ public class DataProcessing {
     
     public final static String[] MAIOR_OU_IGUAL = {">=","=>","\u2265","\u2267"};
     public final static String[] MENOR_OU_IGUAL = {"<=","=<","\u2264","\u2266"};
+    public final static char IGUAL = '=';
+    public final static char MENOS = '-';
+    public final static char MAIS = '+';
     
     //</editor-fold>
     
-    
-    public static String[] getVariaveisDaPrimeiraLinha(String linha){
+    /**
+     * O outpur vai ser um array que contem 3 valores por variavel encontrada
+     * [nome da variavel] [qunatidade] [simbolo de positivo ou negativo]
+     * @param linha
+     * @return 
+     */
+    public static String[][] getVariaveisDaPrimeiraLinha(String linha){
         
-        String[] output = null;
+        String[][] output = null;
         
         if(linha != null){
             
             if(linha.contains("=")){
-        
-                int numDeVariaveis = 0;
 
                 int charIndex = 0;
 
                 while(charIndex < linha.length()){
 
-                    char carater = linha.charAt(charIndex);
-                    
-                    //if(NUMS.contains(carater)){}
-
+                    char caracter = linha.charAt(charIndex);
+                                        
+                    if(eUmaLetra(caracter) && depoisDeUmIgual(linha,charIndex)){
+                      
+                        charIndex = extrairVariavel(charIndex, linha, output);
+                        
+                    }   
                     charIndex++;
                 }
             
 // "Z = 3X1 + 5X2"
             }
+            
         }
+        return output;
+    }
+
+    public static boolean eUmaLetra(char caracter) {
+        
+        boolean output = false;
+
+        if(String.valueOf(caracter).matches("[A-z]")){
+
+            output = true;
+        }
+        
+        return output;
+    }
+
+    public static int extrairVariavel(int charIndex, String linha, String[][] variaveisEncontradas) {
+    
+        //TODO: extrair método
+        //<editor-fold defaultstate="collapsed" desc="Adiciona mais uma posição ao array">
+        
+        if(variaveisEncontradas == null){
+        
+            variaveisEncontradas = new String[1][3];
+                    
+        }else{
+        
+            int numDeVariaveisJaEncontradas = variaveisEncontradas.length;
+            
+            String[][] tempArray = new String[numDeVariaveisJaEncontradas + 1][3];
+            
+            for(int i = 0; i < numDeVariaveisJaEncontradas; i++){
+                
+                tempArray[i] = variaveisEncontradas[i];         
+            } 
+            variaveisEncontradas = tempArray;
+        }
+        
+        //</editor-fold>
+        
+        int idx = charIndex-1;
+        String quantidade = "";
+        char espaco = ' ';
+        while(linha.charAt(idx) != espaco){
+            
+            char carater = linha.charAt(idx);
+            if(String.valueOf(carater).matches("[0-9]")){
+                
+                quantidade = carater + quantidade;
+                
+            }else if(carater == MENOS){
+                //TODO: é negativo
+                break;
+            }else{
+                //TODO é positvo
+            }
+            idx--;
+        }    
+        return charIndex;
+    }
+
+    public static boolean depoisDeUmIgual(String linha, int charIndex) {
+        
+        int idx = linha.indexOf(String.valueOf(IGUAL));
+        System.out.println(idx);
+        boolean output = idx > -1 && idx < charIndex;
+        
         return output;
     }
 }
--- Simplex/test/simplex/DataProcessingTest.java
+++ Simplex/test/simplex/DataProcessingTest.java
@@ -5,11 +5,9 @@
  */
 package simplex;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.assertArrayEquals;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
 /**
@@ -20,35 +18,93 @@ public class DataProcessingTest {
     
     public DataProcessingTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of getVariaveisDaPrimeiraLinha method, of class DataProcessing.
      */
     @Test
     public void testGetVariaveisDaPrimeiraLinha() {
-        
         System.out.println("getVariaveisDaPrimeiraLinha");
-        String linha = "Z = 3X1 + 5X2";
-        String[] expResult = {"X1","X2"};
-        String[] result = DataProcessing.getVariaveisDaPrimeiraLinha(linha);
+        
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+        String linha = "";
+        String[][] expResult = null;
+        String[][] result = DataProcessing.getVariaveisDaPrimeiraLinha(linha);
         assertArrayEquals(expResult, result);
         
     }
+
+    /**
+     * Test of eInicioDeUmaVariavel method, of class DataProcessing.
+     */
+    public void testEUmaLetra() {
+        System.out.println("eInicioDeUmaVariavel");
+        boolean expResult = true;
+        char caracter = 'X';
+        boolean result = DataProcessing.eUmaLetra(caracter);
+        assertEquals(expResult,result);
+    }
+    
+    /**
+     * Test of eInicioDeUmaVariavel method, of class DataProcessing.
+     */
+    public void testEUmaLetraF() {
+        System.out.println("eInicioDeUmaVariavel");
+        boolean expResult = false;
+        char caracter = ' ';
+        boolean result = DataProcessing.eUmaLetra(caracter);
+        assertEquals(expResult,result);
+    }
+    
+    
+    /**
+     * Test of extrairVariavel method, of class DataProcessing.
+     */
+    @Test
+    public void testExtrairVariavel() {
+        System.out.println("extrairVariavel");
+        
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+        int charIndex = 0;
+        String linha = "";
+        String[][] variaveisEncontradas = null;
+        int expResult = 0;
+        int result = DataProcessing.extrairVariavel(charIndex, linha, variaveisEncontradas);
+        assertEquals(expResult, result);
+    }
+
+    //<editor-fold defaultstate="" desc="">
+    
+    /**
+     * Test of depoisDeUmIgual method, of class DataProcessing.
+     */
+    @Test
+    public void testDepoisDeUmIgual() {
+        System.out.println("depoisDeUmIgual");
+        String linha = "Z = 3X1 + 5X2";
+        int charIndex = 3;
+        boolean expResult = true;
+        boolean result = DataProcessing.depoisDeUmIgual(linha, charIndex);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testDepoisDeUmIgualF() {
+        System.out.println("depoisDeUmIgual");
+        String linha = "Z = 3X1 + 5X2";
+        int charIndex = 1;
+        boolean expResult = false;
+        boolean result = DataProcessing.depoisDeUmIgual(linha, charIndex);
+        assertEquals(expResult, result);
+       
+    }
+    //</editor-fold>
+    
+
+   
     
 }
